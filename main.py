33aedf471e7e5dd33c87f416a0583bd03d4ff8ad
Bugraulubay <bugraulubay62@gmail.com>
2025-01-08 14:39:12 +0300

    v1.2 : Detailed Report added

--- main.py
+++ main.py
@@ -6,9 +6,29 @@ def count_words(text):
     word_list = text.split()
     return len(word_list)
 
+def count_characters(text):
+    lowered_text = text.lower()
+    character_dict = {}
+    for c in lowered_text:
+        if c.isalpha():
+            if c in character_dict:
+                character_dict[c] += 1
+            else: character_dict[c] = 1
+    return character_dict
+
+def report(word_count,character_count_dict):
+    print("--- The Report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in document")
+    dict_sorted = dict(sorted(character_count_dict.items(), key=lambda item: item[1], reverse=True))
+    for c in dict_sorted:
+        print(f"The '{c}' character was found {dict_sorted[c]} times")
+    print("--------------- End of Report ---------------")
+
 def main():
     paths = ["books/frankenstein.txt"]
-    word_count = count_words(get_contents(paths[0]))
-    print(word_count)
+    content = get_contents(paths[0])
+    word_count = count_words(content)
+    character_count_dict = count_characters(content)
+    report(word_count,character_count_dict)
 
 main()
